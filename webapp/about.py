f2b77c1ddda751fc6b6d9942e5cb49ee8bfd753e
jigsaw1313 <mr.mhmdi93@gmail.com>
2023-12-18 16:16:39 +0330

    add about page

---
+++ webapp/about.py
@@ -0,0 +1,27 @@
+import justpy as jp
+
+
+class About:
+    path = "/about"
+
+    def serve(self):
+        wp = jp.QuasarPage(tailwind=True)
+        div = jp.Div(a=wp, classes='bg-gray-300 h-screen')
+        jp.Div(a=div, text="This is the About Page", classes='text-4xl m2')
+        jp.Div(a=div, text="""
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+            totam rem aperiam, eaque ipsa quae ab illo inventore veritatis 
+            et quasi architecto beatae vitae dicta sunt explicabo. 
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, 
+            sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, 
+            adipisci velit, sed quia non numquam eius modi tempora
+            incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam. 
+            """, classes="text-lg")
+
+        return wp
+
+
+jp.Route(About().path, About.serve)
+jp.justpy(port=8001)
